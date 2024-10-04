56eebf3e9d97ed9efc5b62de05a0e19f1d7bc2fd
Givaldo Santos <49195218+givamarques@users.noreply.github.com>
2024-10-04 01:00:48 -0300

    FEAT: edicao redes

---
+++ scripts.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("leiamais");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Leia mais";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Leia menos";
+      moreText.style.display = "inline";
+    }
+  }
+
+  fetch('poo.html')
+         .then(response => response.text())
+         .then(data => {
+             document.getElementById('poo').innerHTML = data;
+         })
+         .catch(error => console.error('Erro ao carregar o arquivo:', error));
